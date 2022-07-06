d589bf1aebed04f48793f6a98d68e362d391ba7f
frontyoon <issou06261@gmail.com>
2022-07-06 11:58:07 +0900

    initial commit

--- taste_trevel/urls.py
+++ taste_trevel/urls.py
@@ -1,4 +1,4 @@
-"""taste_strip URL Configuration
+"""taste_trevel URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
